8a96f77a691e9d68d8c581a97b0c0d99118b7513
Tim Kao <pakuya.kao@gmail.com>
2019-07-06 11:06:52 -0700

    first look at layout manager

--- app/src/main/java/com/example/travelapp/FlagActivity.java
+++ app/src/main/java/com/example/travelapp/FlagActivity.java
@@ -3,6 +3,7 @@ package com.example.travelapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -17,6 +18,12 @@ public class FlagActivity extends AppCompatActivity {
         Button butt =  new Button(this);
         butt.setText("my button");
         LinearLayout layout = findViewById(R.id.flagLayout);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        butt.setLayoutParams(params);
+
         layout.addView(butt, 1);
 
     }
