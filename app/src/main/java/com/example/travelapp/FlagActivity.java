f46556a2dccdee243081cfa0f64171b423c7f795
Tim Kao <pakuya.kao@gmail.com>
2019-07-06 13:01:56 -0700

    layour inflater with a bug

--- app/src/main/java/com/example/travelapp/FlagActivity.java
+++ app/src/main/java/com/example/travelapp/FlagActivity.java
@@ -3,31 +3,50 @@ package com.example.travelapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class FlagActivity extends AppCompatActivity {
+
+    private static final String[] COUNTRIES = {
+            "USA",
+            "Japan",
+            "Taiwan",
+            "Spain"
+    };
+
+    private void addFlag(String countryName, GridLayout layout) {
+        View flag = getLayoutInflater().inflate(R.layout.flag, layout);
+        System.out.println(countryName);
+
+
+        // IMPORTANT!! should be flag.findViewById (inside flag.xml) not just findViewById (whole activity)
+        TextView tv = flag.findViewById(R.id.flagTxt);
+        tv.setText(countryName);
+
+        ImageView imgv = flag.findViewById(R.id.flagImg);
+        int flagImgId = getResources().getIdentifier(countryName.toLowerCase(),"drawable", getPackageName());
+        imgv.setImageResource(flagImgId);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flag);
 
-        // add a new button
-        Button butt =  new Button(this);
-        butt.setText("my button");
-        LinearLayout layout = findViewById(R.id.flagLayout);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        );
-        params.gravity = Gravity.CENTER;
-        params.weight = 1;
-        butt.setLayoutParams(params);
+        GridLayout layout = findViewById(R.id.flagLayout);
 
-        layout.addView(butt, 1);
+        for (String country : COUNTRIES) {
+            addFlag(country, layout);
+        }
 
     }
 }
